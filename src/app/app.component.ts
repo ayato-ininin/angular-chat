c6f7c8915b4a4ea54aaf36de72c3519e8837bf95
ayato-ininin <sarai.a.1203408@gmail.com>
2021-08-17 15:05:08 +0900

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList, SnapshotAction } from '@angular/fire/database';
 import { Observable } from 'rxjs';
+import {map} from 'rxjs/operators'
 
 import { Comment } from './class/comment';
 import { User } from './class/user';
@@ -9,12 +10,7 @@ import { User } from './class/user';
 const CURRENT_USER: User = new User(1, '五十川洋平');
 const ANOTHER_USER: User = new User(2, '竹井賢治');
 
-const COMMENTS: Comment[] = [
-  new Comment(ANOTHER_USER, 'お疲れさまです。' ) ,
-  new Comment(ANOTHER_USER, 'この間の件ですが、どうなりましたか？'),
-  new Comment(CURRENT_USER, 'お疲れさまです。'),
-  new Comment(CURRENT_USER, 'クライアントからOK。' )
-]
+
 
 @Component({
   selector: 'ac-root',
@@ -22,7 +18,10 @@ const COMMENTS: Comment[] = [
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  comments = COMMENTS;
+  comments$: Observable<Comment[]>;
+  commentsRef: AngularFireList<Comment>;
+  // angularfirelistにすると、db.listの値が受け取れる。List参照が格納
+
   currentUser = CURRENT_USER;
   comment = "";
   item$: Observable<any>;
@@ -30,11 +29,31 @@ export class AppComponent {
   constructor(private db: AngularFireDatabase) {
     this.item$ = db.object('/item').valueChanges();
     // これを指定することで、リアルタイムデータベースでitemから単一データを取得できて、observableに変える。
+    this.commentsRef = db.list('/comments');
+    // this.comments$ = this.commentsRef.valueChanges();
+    // ただ、このvalueChangesはlistの実データのみ取得して、keyを取得していない。これじゃ更新作業とかができない。↓↓
+    this.comments$ = this.commentsRef.snapshotChanges()
+      .pipe(
+        map((snapshots: SnapshotAction<Comment>[]) => {
+          // まずここでkeyを含めたメタデータを取得
+          return snapshots.map(snapshot => {
+            const value = snapshot.payload.val();
+             // payload.val、ー＞ここで実データを取得・
+            return new Comment({ key: snapshot.payload.key, ...value });
+            // このcomments$には、新たに、今までの値をvalueに含め、keyという新たなプロパティを足して、配列を作り変えている！！！！
+            // コレ自体、pushしてないから、多分データには反映されない。
+            // 追加されたcommentsRefにkeyを追加して、もう一回配列を入れている！！
+        })
+      })
+    )
+
   }
 
   addComment(comment: string) {
     if (comment) {
-      this.comments.push(new Comment(this.currentUser, comment));
+      this.commentsRef.push(new Comment({ user:this.currentUser, message:comment }));
+      // AngularFireDatabaseにpushすることで、list追加になる。db.listにすると、値の追加とかが可能。
+      this.comment = "";
     }
 
   }
--- src/app/class/comment.ts
+++ src/app/class/comment.ts
@@ -2,8 +2,19 @@ import { User } from './user';
 
 export class Comment {
   date: number;
-  constructor(public user: User, public message: string) {
-    this.date = Date.now();
+  user: User;
+  message: string;
+  key?: string;
+  isEdit?: boolean;
+
+  constructor(value: any) {
+    // valueにすることで、何でも受け取れるようにしてる。実際はオブジェクトが来る
+    this.user = value.user;
+    this.message = value.message;
+    this.date = value.data || Date.now();
+    if (value.key) {
+      this.key = value.key;
+    }
   }
 
 }
